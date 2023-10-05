15cfc1c0979638a195590aaed3b0c34cab270017
momenbh <mdmomenbhuiya@gmail.com>
2023-10-05 16:29:10 +0600

    problem-fix

--- resources/views/frontend/fixed/nav.blade.php
+++ resources/views/frontend/fixed/nav.blade.php
@@ -3,6 +3,6 @@
         <a href="{{ route('home') }}" class="navbar-brand">
             <img class="img-fluid" src="{{ url('images/logo.png') }}" alt="China Bazar Blog" width="148" height="50">
         </a>
-        <p style="color:#FF2900" class="mx-auto">ChinaBazarB2B Blog</p>
+        <p style="color:#FF2900;font-size:23px;font-weight:bold" class="mx-auto">ChinaBazarB2B Blog</p>
     </div>
 </nav>
