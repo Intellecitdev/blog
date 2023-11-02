dea6703916db6511401d416e0eca872361c04c38
momenbh <mdmomenbhuiya@gmail.com>
2023-11-02 17:41:06 +0600

    fix-problem

--- resources/views/frontend/layout.blade.php
+++ resources/views/frontend/layout.blade.php
@@ -9,6 +9,7 @@
     <link rel="canonical" href="{{ url()->current() }}" />
     <meta name="description" content="@yield('meta_description')" />
     <meta name="author" content="@yield('author')" />
+    <meta name="google-site-verification" content="8Di8r-WtxyHrQYXgcCSvBAo2eNLE3GLFriCrhBSRWaI" />
     <title>@yield('title')</title>
     <!-- Favicon-->
     <link rel="shortcut icon" href="https://www.chinabazarb2b.com/img/frontend/brand/favicon-32x32.png"
@@ -20,6 +21,12 @@
     <link href="{{ url('/frontend/css/styles.css') }}" rel="stylesheet" />
     {{-- font awesome cdn --}}
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+   
+    <!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KPPPKW96"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+
 </head>
 
 <body>
@@ -42,6 +49,15 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
     <!-- Core theme JS-->
     <script src="{{ url('frontend/js/scripts.js') }}"></script>
+     
+    <!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KPPPKW96');</script>
+<!-- End Google Tag Manager -->
+
 </body>
 
 </html>
